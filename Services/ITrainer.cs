aeaeac1dcc96e9c499acde737f9bd3069ea505be
Saul_Berrenson <pkspart@yandex.ru>
2021-01-10 18:25:50 +0300

    Interface for general methods

---
+++ Services/ITrainer.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace NboxTrainer.Services
+{
+    public interface ITrainer
+    {
+        public Task<bool> Train();
+        public Task SaveModel();
+    }
+}
