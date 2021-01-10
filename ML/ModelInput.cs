fe902231bc468cfa0bd7ba3b4190d998f2b1878f
Saul_Berrenson <pkspart@yandex.ru>
2021-01-10 20:11:27 +0300

    Create classes for Predictors

---
+++ ML/ModelInput.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NboxTrainer.ML
+{
+    public class ModelInput
+    {
+        public byte[] Image { get; set; }
+
+        public UInt32 LabelAsKey { get; set; }
+
+        public string ImagePath { get; set; }
+
+        public string Label { get; set; }
+    }
+}
---
+++ ML/ModelOutput.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Linq;
+
+namespace NboxTrainer.ML
+{
+    public class ModelOutput
+    {
+        public string ImagePath { get; set; }
+
+        public string Label { get; set; }
+
+        public string PredictedLabel { get; set; }
+
+        public float[] Score { get; set; }
+
+        public override string ToString()
+        {
+            return $"File:   {new FileInfo(ImagePath)?.Name} |  Predicted: {PredictedLabel}  |    Accuracy: {Score?.Max()}";
+        }
+    }
+}
